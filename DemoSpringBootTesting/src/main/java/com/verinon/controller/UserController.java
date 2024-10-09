fb1a04b60207fa034d00472ff1edca0abfae6d60
RajkumarMekala <rajkumar.mekala@verinon.co.in>
2024-10-09 11:09:46 +0530

    Added Controller Class

---
+++ DemoSpringBootTesting/src/main/java/com/verinon/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.verinon.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+	
+	public String users() {
+		return "Hello......";
+		
+	}
+
+}
