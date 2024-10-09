115abb4dfda891cf0310b31a77c5d5c28bbd72f2
RajkumarMekala <rajkumar.mekala@verinon.co.in>
2024-10-09 10:07:25 +0530

    Created User Class

---
+++ DemoSpringBootTesting/src/main/java/com/verinon/model/User.java
@@ -0,0 +1,23 @@
+package com.verinon.model;
+
+public class User {
+	private String userId;
+	private String UserName;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return UserName;
+	}
+
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+
+}
